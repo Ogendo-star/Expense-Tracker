c1b66262e472fb97ba4c3c9c47021b0296d29d3d
Masela Ogendo <puroselah@gmail.com>
2025-04-14 06:48:54 +0300

    Feat:add values to the option tag

--- src/components/ExpenseFilter.jsx
+++ src/components/ExpenseFilter.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const ExpenseFilter = () => {
   return (
     <select name="" id="" className="form-select">
-        <option value=""></option>
+        <option value="toiletries">Toiletries</option>
+        <option value="savings">Savings</option>
+        <option value="personal care">Personal care</option>
+        <option value="transport">Tansport</option>
+        <option value="food">Food</option>
     </select>
   )
 }
